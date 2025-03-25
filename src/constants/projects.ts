33b2452001f3dc1532ac6e86c6ee4717a7114067
peterbeeshop <muyambangopeter@gmail.com>
2025-03-25 22:05:22 +0200

    initial commit

---
+++ src/constants/projects.ts
@@ -0,0 +1,80 @@
+export const projects = [
+  {
+    title: "Stock App",
+    description:
+      "A stock app that allows users to add a particular stock to their watchlist or portfolio, so that they can track it's performance overtime and see what their returns would be. I also integrated into the website a payment gateway (pay-pal) and implemented user authentication and authorization using JSON Web Tokens.",
+    image: "/images/stock-app.png",
+    tags: [
+      "React",
+      "Typescript",
+      "Redux toolkit",
+      "Express js",
+      "Google OAuth",
+      "React query",
+      "SASS",
+    ],
+    source: "https://wallstreetfinds.netlify.app/",
+    visit: "https://github.com/peterbeeshop/react-stock-app",
+    id: 1,
+  },
+  {
+    title: "Hulu 2.0",
+    description:
+      "This is a clone of the official Hulu website. The data is powered to us by the tmdb.org api, which provides us with upto date movies. Tailwind css and tailwind JIT were used to style the app, and css grid to display the movies according to screen size.",
+    image: "/images/hulu.png",
+    tags: ["Next js", "Tailwind css", "API's"],
+    source: "https://petershuluclone.netlify.app",
+    visit: "https://github.com/peterbeeshop/hulu-clone",
+    id: 2,
+  },
+  {
+    title: "Modern Landing Page",
+    description:
+      "A modern looking landing page from figma design to developement, using React js",
+    image: "/images/gpt3.png",
+    tags: ["React Js", "Tailwind css", "React Icons", "Figma"],
+    source: "https://modernlandingpage.netlify.app/",
+    visit: "https://github.com/peterbeeshop/GPT-3-landing-page",
+    id: 3,
+  },
+  {
+    title: "Disney plus",
+    description:
+      "A Clone of the official Disney plus website. Built using React, with the help of functional components, styled-components, react-redux. Uses google authentication to authenticate the user, with the help of firebase.",
+    image: "/images/disneyplus.png",
+    tags: ["React", "Firebase", "React-Redux"],
+    source: "https://disneyplusclone-9c5fb.web.app/home",
+    visit: "https://github.com/peterbeeshop/disneyClone-app",
+    id: 4,
+  },
+  // {
+  //   title: "Node Js Authentication",
+  //   description:
+  //     "In this app, in order for you to access certain pages you need to be a logged in user otherwise you would be restricted from accessing those pages. (App takes 30secs for it to load due to my free hosting service. Kindly be patient with it)",
+  //   image: "/images/jwt.png",
+  //   tags: ["Node Js", "Express", "MongoDb", "Heroku"],
+  //   source: "https://jwt-authentication-app.herokuapp.com/",
+  //   visit: "https://github.com/peterbeeshop/jwt-auth",
+  //   id: 5,
+  // },
+  {
+    title: "Black-Jack Game (For PC only)",
+    description:
+      "A Javascript game where you pick random cards between 1 to 11 for as long as you want but the total score of the cards picked should not be over 21. You get to play against a bot. The person that has the bigger score wins.. HIT randomly generates a card for you. STAND will randomly generate cards for the bot (This game was created over 3 years ago when I just started with coding)",
+    image: "/images/blackjack.png",
+    tags: ["Vanilla JavaScript"],
+    source: "https://blackjackjsgame.netlify.app/",
+    visit: "https://github.com/peterbeeshop/blackjack-game",
+    id: 5,
+  },
+  {
+    title: "Landing Page",
+    description:
+      "Whether a small company or big business enterprise, we all need a good landing page to entice our users to continue exploring our site even further. This is one of those types of landing pages that your business/company might do with any given day.",
+    image: "/images/laptopwebsite.png",
+    tags: ["React", "Next Js", "Figma"],
+    source: "https://landingpagebypeter.netlify.app/ ",
+    visit: "https://github.com/peterbeeshop/laptopwebsite",
+    id: 6,
+  },
+];
---
+++ src/constants/timeline.ts
@@ -0,0 +1,22 @@
+export const TimeLineData = [
+  {
+    year: 2020,
+    text: "Worked as fullstack intern for a Nigerian company from March of 2020 - Dec 2020",
+  },
+  {
+    year: 2021,
+    text: "Worked for Joyn Technologies, a canadian startup from Jan 2021 - Sep 2022",
+  },
+  {
+    year: 2022,
+    text: "Started working for Ruta Health, a canadian startup from Sep 2022 - Dec 2023 ",
+  },
+  {
+    year: 2024,
+    text: "Started working on Moment, a social network that will leverage Virtual Reality , Augmented Reality and generative AI going forward",
+  },
+  {
+    year: 2024,
+    text: "Senior FullStack Developer & DevOps Lead at Yamfumu Technology & Marketing Agency",
+  },
+];
